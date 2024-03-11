b230531e70c4fb41ec57fda2f36142489022a4de
Predeep <predeepkumar.us2022cse@sece.ac.in>
2024-03-11 15:15:12 +0530

    Changes

--- src/components/PasswordList.tsx
+++ src/components/PasswordList.tsx
@@ -49,8 +49,8 @@ export default function PasswordList({ search }: Props) {
       ) : (
         filteredPasswords.map(password => (
           <Link to={`/pwd/get-all/${password.id}`} key={password.id}>
-            <div className="bg-gray-200 rounded-lg p-4 mt-6 flex flex-row justify-between">
-              <h2 className="text-xl font-bold hover:text-blue-600">{password.title}</h2>
+            <div className="bg-gray-200 rounded-lg p-4 mt-6 flex flex-row justify-between hover:text-blue-600">
+              <h2 className="text-xl font-bold">{password.title}</h2>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="text-blue-600 lucide lucide-log-in"><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"/><polyline points="10 17 15 12 10 7"/><line x1="15" x2="3" y1="12" y2="12"/></svg>
             </div>
           </Link>
--- src/components/Vault.tsx
+++ src/components/Vault.tsx
@@ -1,52 +1,52 @@
-import React, { useState } from 'react';
-import PasswordList from './PasswordList';
-import { Link } from 'react-router-dom';
+  import React, { useState } from 'react';
+  import PasswordList from './PasswordList';
+  import { Link } from 'react-router-dom';
 
-function Vault() {
-  const [search, setSearch] = useState<string>('');
+  function Vault() {
+    const [search, setSearch] = useState<string>('');
 
-  return (
-    <div className="flex flex-col h-screen items-center justify-center bg-gray-200">
-      <div className="max-w-md w-full bg-white rounded-lg shadow-lg p-6 max-h-[100vh]">
-        <h1 className="text-center text-3xl font-bold mb-4">SECURE VAULT</h1>
-        <div className="flex flex-row gap-2">
-          <input
-            type="search"
-            id="search-box"
-            placeholder="Search here..."
-            className="w-full rounded-md border px-4 py-2 focus:outline-blue-500"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <button
-            type="button"
-            id="search-button"
-            className="bg-blue-500 py-2 px-4 text-white rounded-md hover:bg-blue-700"
-            onClick={() => {
-              // Perform search functionality here
-              console.log('Search:', search);
-            }}
-          >
-            Search
-          </button>
-        </div>
-        <div id="password-list" className="max-w-sm w-full max-h-[400px] overflow-y-auto  bg-white p-3 rounded-lg shadow-sm" style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
-          <PasswordList search={search} />
-        </div>
-        <div className="flex justify-between max-w-sm w-full mt-4">
-          <Link to="/">
-            <button id="logout" className="font- max-w-24 rounded-md bg-blue-500 p-2 text-white hover:animate-pulse hover:bg-blue-700">Log Out</button>
-          </Link>
-          <Link to="/add">
-            <button id="add" className="font-max-w-24 rounded-md bg-blue-500 p-2 text-white hover:animate-pulse hover:bg-blue-700">Add</button>
-          </Link>
-          <Link to="/Pwd">
-            <button id="logout" className="font-max-w-24 rounded-md bg-blue-500 p-2 text-white hover:animate-pulse hover:bg-blue-700">Generate</button>
-          </Link>
+    return (
+      <div className="flex flex-col h-screen items-center justify-center bg-gray-200">
+        <div className="max-w-md w-full bg-white rounded-lg shadow-lg p-6 max-h-[100vh]">
+          <h1 className="text-center text-3xl font-bold mb-4">SECURE VAULT</h1>
+          <div className="flex flex-row gap-2">
+            <input
+              type="search"
+              id="search-box"
+              placeholder="Search here..."
+              className="w-full rounded-md border px-4 py-2 focus:outline-blue-500"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <button
+              type="button"
+              id="search-button"
+              className="bg-blue-500 py-2 px-4 text-white rounded-md hover:bg-blue-700"
+              onClick={() => {
+                // Perform search functionality here
+                console.log('Search:', search);
+              }}
+            >
+              Search
+            </button>
+          </div>
+          <div id="password-list" className="max-w-sm w-full max-h-[400px] overflow-y-auto  bg-white p-3 rounded-lg shadow-sm" style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
+            <PasswordList search={search} />
+          </div>
+          <div className="flex justify-between max-w-sm w-full mt-4">
+            <Link to="/">
+              <button id="logout" className="font- max-w-24 rounded-md bg-blue-500 p-2 text-white hover:animate-pulse hover:bg-blue-700">Log Out</button>
+            </Link>
+            <Link to="/add">
+              <button id="add" className="font-max-w-24 rounded-md bg-blue-500 p-2 text-white hover:animate-pulse hover:bg-blue-700">Add</button>
+            </Link>
+            <Link to="/Pwd">
+              <button id="logout" className="font-max-w-24 rounded-md bg-blue-500 p-2 text-white hover:animate-pulse hover:bg-blue-700">Generate</button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default Vault;
+  export default Vault;
